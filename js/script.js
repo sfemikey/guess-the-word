262695bff2ff00dae1a9cd65dff8e57542e650fd
sfemikey <femikeyme01@yahoo.com>
2021-04-26 21:37:12 -0600

    Display word and guessed letters on the screen

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const guessedLettersList = document.querySelector(".guessed-letters");
 const guessLetterButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
-const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
-const messageText = document.querySelector(".message");
+const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
@@ -26,7 +26,7 @@ placeholder(word);
 guessLetterButton.addEventListener("click", function (e) {
     //prevent the default behavior of clicking a button
     e.preventDefault();   
-    messageText.innerText = "";
+    message.innerText = "";
     //grab what was entered in the input
     const guess = letterInput.value;
     //validate to make sure that it is a single letter
@@ -44,14 +44,14 @@ const validateInput = function (input) {
     //use conditional block to check for different scenarios
     if (input.length === 0) {
         //if input is empty
-        messageText.innerText = "Please enter a letter.";  
+        message.innerText = "Please enter a letter.";  
     } else if (input.length > 1) {
         //if input is more than one letter
-        messageText.innerText = "Please enter single letter.";  
+        message.innerText = "Please enter single letter.";  
     } else if (!input.match(acceptedLetter)) {
         //if they've entered a character that doesn't match the regular expression pattern
-        messageText.innerText = "Please enter a letter from A to Z.";  
-    }else {
+        message.innerText = "Please enter a letter from A to Z.";  
+    } else {
         //If all the other conditions aren't met
      return input;  
     }
@@ -67,9 +67,57 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        //Call the function inside the else statement
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
     }
-
 };
+
+//Create a Function to Show the Guessed Letters
+const showGuessedLetters = function () {
+    //empty unordered list
+    guessedLettersList.innerHTML = "";
+    for (const letter of guessedLetters) {
+        //Create a new list item for each letter inside your guessedLetters array
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersList.append(li);
+    }
+};
+
+//Create a Function to Update the Word in Progress
+const updateWordInProgress = function (guessedLetters) {
+    // change the word variable to uppercase
+    const wordUpper = word.toUpperCase();
+    //create a variable to split the word string into an array
+    const wordArray = wordUpper.split("");
+    const showWord = [];
+    for (const letter of wordArray) {
+        //if the wordArray contains any letters from the guessedLetters array
+        if (guessedLetters.includes(letter)) {
+            //update the circle symbol with the correct letter.
+            showWord.push(letter.toUpperCase());
+        } else {
+            showWord.push("●");
+        }
+    }
+    //console.log(showWord);
+    //use join() to update the empty paragraph where the word in progress will appear.
+    wordInProgress.innerText = showWord.join("");
+    //call this function to check if the player has won
+    checkIfPlayerWon();
+};
+
+//Create a Function to Check If the Player Won
+const checkIfPlayerWon = function () {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        //add the "win" class to the empty paragraph
+        message.classList.add("win");
+        //update the paragraph's contents
+        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+    }
+};
+
 
 
 
